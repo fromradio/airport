7ebbf20d9f6294e81e0619eb0401aa274555cd2d
Ruimin Wang <ruimin.wang13@gmail.com>
2016-10-11 21:01:13 +0800

    back to home

--- src/ave_time.py
+++ src/ave_time.py
@@ -1,25 +1,35 @@
+#! /bin/env python
 
-with open("t1_t2_area.csv.gst",'r') as f:
-    n = 0
-    sum = 0.0
-    line = f.readline()
-    while True:
-        line = f.readline()
-        if line:
-            sdata = line.strip().split()
-            if sdata[1] != "00.00":
-                n = n + 1
-                diff = float(sdata[1]) - float(sdata[0])
-		# print diff,sum
-		# raw_input()
-                sum = sum + diff
-            else:
-                continue
-        else:
-            break
+import pandas as pd
+import numpy as np
 
-    res = sum/n
-    print "average time is :",res
+filePath = "../data/t1_t2_area.csv"
+data = pd.read_csv(filePath, sep = "\t")
+data.replace(0.00, np.NaN)
+print data
+
+valid_data = data.dropna()
+print pd.to_datetime(valid_data['flytime'], format = "%H.%M")
+
+# with open("t1_t2_area.csv.gst", 'r') as f:
+#     n = 0
+#     sum = 0.0
+#     line = f.readline()
+#     while True:
+#         line = f.readline()
+#         if line:
+#             sdata = line.strip().split()
+#             if sdata[1] != "00.00":
+#                 n = n + 1
+#                 diff = float(sdata[1]) - float(sdata[0])
+#                 sum = sum + diff
+#             else:
+#                 continue
+#         else:
+#             break
+#
+#     res = sum/n
+#     print "average time is :",res
 
 # average time is : 1.31939000984
 
